27c94d8785383d503b9dfb9f9d8be8149cb776de
LFedorovQA <gsergzs11@gmail.com>
2026-03-09 14:14:23 +0300

    Lesson1 тестовый коммит

--- Lesson_1/Lesson_1_1.py
+++ Lesson_1/Lesson_1_1.py
@@ -1,1 +1,16 @@
-print('Hello, World!')
+# a = b = c = 0
+# a, b, c = 0, 0, 0
+
+
+a, b = 8, 10.89
+# print(a)
+# print(b)
+print(type(b))
+
+
+a = b = 4
+c, d = 5, 6
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(d))
