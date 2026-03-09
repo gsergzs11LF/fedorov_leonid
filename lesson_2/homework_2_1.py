300bfcb4d955f05de9cef86d5678cf01899e2ebe
LFedorovQA <gsergzs11@gmail.com>
2026-03-09 17:04:02 +0300

    homework по первому уроку

---
+++ lesson_2/homework_2_1.py
@@ -0,0 +1,49 @@
+#задание 1
+a = 8
+b = 3
+x = 2.5
+y = -1.7
+print(type(a), a)
+print(type(b), b)
+print(type(x), x)
+print(type(y), y)
+
+#задание 2
+num1 = 5
+num2 = 10
+print("Сложение: ", num1 + num2)
+print("Вычитание: ", num1 - num2)
+print("Умножение: ", num1 * num2)
+print("Деление: ",num1 / num2)
+print("Целочисленное деление: ",num1 // num2)
+print("Остаток от деления: ",num1 % num2)
+print("Возведение в степень: ",num1 ** num2)
+
+#задание 3
+a = 10
+b = 3
+c = -10
+d = -3
+print(a / b)
+print(a // b)
+print(a % b)
+print(c / d)
+print(c // d)
+print(c % d)
+
+#задание 4
+a = 5 + 3 * 2
+b = (5 + 3) * 2
+c = 10 / 2 ** 2
+print('Выражение 1: ', a)
+print('Выражение 2: ', b)
+print('Выражение 3: ', c)
+
+#задание 5
+count = 10
+count += 5
+count -= 3
+count *= 2
+count /= 4
+print("Итоговое значение Count: ", count)
+
---
+++ lesson_2/homework_2_2.py
@@ -0,0 +1,54 @@
+#задание 1
+s1 = 'Python'
+s2 = 'Программирование'
+print(s1, s2)
+s3 = '''
+вот такая большая многострочная строка
+можно на несколько строк выводить
+и будет отлично вообще
+'''
+print(s3)
+empty = ''
+print("Длина пустой строки: ", len(empty))
+
+#задание 2
+first_name = "Иван"
+last_name = "Петров"
+full_name = first_name + ' ' + last_name
+print('Полное имя с пробелом:', full_name)
+
+#задание 3
+s = 'Возраст: '
+age = 25
+print(s + str(age))
+
+#задание 4
+a = 'ха'
+print('ха' * 4)
+# print('ха' * 2.5) тут будет ошибка, так как только на целые числа можно умножать
+
+#задание 5
+text = 'Привет, мир!'
+print("Длина строки: ", len(text))
+text2 = ""
+print("Длина строки2: ", len(text2))
+
+#задание 6
+sentence = "Я изучаю Python"
+print('Python' in sentence)
+print('Java' in sentence)
+
+#задание 7
+a1 = 'apple'
+b1 = 'banana'
+print('apple == banana: ', a1 == b1)
+print('apple != banana: ', a1 != b1)
+print('apple < banana: ', a1 < b1)
+print('apple > banana: ', a1 > b1)
+print('apple >= banana: ', a1 >= b1)
+print('apple <= banana: ', a1 <= b1)
+
+#задание 8
+print(ord("А"))
+print(ord("а"))
+print(ord("Я"))
